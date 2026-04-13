dc49778297c75ded61478cb3c65d5e658c94eb4b
matioli_cptm <guilhermemsbr@gmail.com>
2026-04-13 16:46:32 -0300

    conselho intermediario update

--- conselho_de_classe/src/components/conselho-intermediario/conselho-intermediario.jsx
+++ conselho_de_classe/src/components/conselho-intermediario/conselho-intermediario.jsx
@@ -19,8 +19,10 @@ export function ConselhoIntermediario() {
         <div className="content_conselho_intermediario">
             <div className="cards_turma_info">
                 <div className="card_total_alunos">
-                    <h3 className="int_total_alunos">{intTotalAlunos}</h3>
-                    <p className="text_total_alunos">Total de Alunos</p>
+                    <div className="int_text_total_alunos">
+                        <h3 className="int_total_alunos">{intTotalAlunos}</h3>
+                        <p className="text_total_alunos">Total de Alunos</p>
+                    </div>
                     <div className="icon">
                         <img src={totalAlunosIcon} alt="" />
                     </div>
@@ -61,26 +63,32 @@ export function ConselhoIntermediario() {
                 </div>
             </div>
             <div className="card_alunos">
-                <div className="checkbox_selecionar_tudo">
-                    <input type="checkbox" name="" id="" />
-                    <p>Selecionar Tudo</p>
-                </div>
-                <div className="quatidade_alunos_selecionados">
-                    <p>Alunos Selecionados: 0</p>
+                <div className="top_tabela_alunos">
+                    <div className="checkbox_selecionar_tudo">
+                        <input type="checkbox" name="" id="" />
+                        <p>Selecionar Tudo</p>
+                    </div>
+                    <div className="quatidade_alunos_selecionados">
+                        <p>Alunos Selecionados: 0</p>
+                    </div>
                 </div>
                 <div className="tabela_alunos">
-                    <div className="aluno">
-                        <input type="checkbox" name="" id="" />
-                        <p>Jorge Marques de Salves</p>
-                        <img src={notificationIcon} alt="" />
-                        <div className="div_observacoes">
-                            <p>Ver Observações</p>
-                            <p>({intObservacoes})</p>
+                    <div className="div_aluno">
+                        <div className="div_aluno_info">
+                            <input type="checkbox" name="" id="" />
+                            <p>Jorge Marques de Salves</p>
+                            <img src={notificationIcon} alt="" />
+                            <div className="div_observacoes">
+                                <p>Ver Observações</p>
+                                <p>({intObservacoes})</p>
+                            </div>
                         </div>
-                        <button className="btn_registro"></button>
+                        <div className="div_btn_restrito">
+                            <button className="btn_restrito">Restrito</button>
+                        </div>
                         
-
                     </div>
+                    
                 </div>
             </div>
         </div>
