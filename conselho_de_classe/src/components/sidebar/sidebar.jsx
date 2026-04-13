e09c36b7b868e7c050b859db045552ba54b0dcb2
matioli_cptm <guilhermemsbr@gmail.com>
2026-04-13 15:05:16 -0300

    sidebar att - start tela conselho intermediario

--- conselho_de_classe/src/components/sidebar/sidebar.jsx
+++ conselho_de_classe/src/components/sidebar/sidebar.jsx
@@ -1,6 +1,10 @@
 import './sidebar-style.css'
 import dashboardIcon from '../../assets/dashboard-icon.svg';
-import arrowRightIcon from '../../assets/right-arrow-icon.svg'
+import councilIcon from '../../assets/council-icon.svg';
+import reportIcon from '../../assets/report-icon.svg';
+import configIcon from '../../assets/config-icon.svg';
+
+import arrowRightIcon from '../../assets/right-arrow-icon.svg';
 
 export function Sidebar() {
 
@@ -13,19 +17,21 @@ export function Sidebar() {
                     <p>Dashboard</p>
                     <img src={arrowRightIcon} alt="" />
                 </button>
-                <button className="dashboard_button">
-                    <img src={dashboardIcon} alt="" />
+                <button className="coucil_btn">
+                    <img src={councilIcon} alt="" />
+                    <p>Conselhos</p>
+                    <img src={arrowRightIcon} alt="" />
+                </button>
+                <button className="report_btn">
+                    <img src={reportIcon} alt="" />
                     <p>Dashboard</p>
                     <img src={arrowRightIcon} alt="" />
                 </button>
-                <button className="dashboard_button">
-                    <img src={dashboardIcon} alt="" />
-                    <p>Dashboard</p>
-                    <img src={arrowRightIcon} alt="" />
-                </button><button className="dashboard_button">
-                    <img src={dashboardIcon} alt="" />
-                    <p>Dashboard</p>
-                    <img src={arrowRightIcon} alt="" />
+            </div>
+            <div className="div_config">
+                <button className="config_btn">
+                    <img src={configIcon} alt="" />
+                    <p>Configurações</p>
                 </button>
             </div>
         </div>
