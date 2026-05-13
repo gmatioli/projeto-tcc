642854fad0c0bb70de847ad5e6d1d4484ceea256
matioli_senai <guilhermemsbr@gmail.com>
2026-05-13 16:54:52 -0300

    feat(ui): criação variavel cor senai

--- conselho_de_classe/src/components/header/Header.jsx
+++ conselho_de_classe/src/components/header/Header.jsx
@@ -49,7 +49,7 @@ export function Header() {
     }, [navigate]);
 
   return (
-      <header className='h-[8vh] bg-[#b91d32] flex justify-between items-center'>
+      <header className='h-[8vh] bg-[var(--red-senai)] flex justify-between items-center'>
         <div className="ml-7">
           <img onClick={() => navigate('/docente/home')}  src="/img/logo-senai-branco.png" alt="Logo da instituição SENAI" className='w-40 h-11 ml-10 cursor-pointer opacity-100' />
         </div>
