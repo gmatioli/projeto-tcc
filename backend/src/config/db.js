1d7355f23b92d7dc1575bb002dab6d1243a08c00
matioli_senai <guilhermemsbr@gmail.com>
2026-05-22 16:05:43 -0300

    feat(ux): correção servidor

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,16 @@
-const { neon } = require('@neondatabase/serverless');
+/* const { neon } = require('@neondatabase/serverless');
 require('dotenv').config();
 
 const db = neon(process.env.DATABASE_URL);
+
+module.exports = db;
+ */
+
+const postgres = require('postgres');
+
+// Ele vai buscar a variável DATABASE_URL que vamos configurar lá no painel do Render
+const db = postgres(process.env.DATABASE_URL, {
+  ssl: 'require', // Isso é OBRIGATÓRIO para o Neon funcionar na nuvem
+});
 
 module.exports = db;
--- backend/src/server.js
+++ backend/src/server.js
@@ -28,10 +28,25 @@ app.use('/observacoes', require('./routes/observacoes'));
 // ==========================================
 // INICIALIZAÇÃO DO SERVIDOR
 // ==========================================
-const db = require('./config/db');
+/* const db = require('./config/db');
 
 app.listen(3001, async () => {
   console.log('Servidor rodando na porta 3001');
+  try {
+    await db`SELECT 1`;
+    console.log('Conectado ao banco de dados (Neon) com sucesso!');
+  } catch (erro) {
+    console.error('Falha ao conectar ao banco de dados:', erro.message);
+  }
+}); */
+
+const db = require('./config/db');
+
+// O Render vai usar process.env.PORT. O 3001 fica como fallback para sua máquina local.
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, async () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
   try {
     await db`SELECT 1`;
     console.log('Conectado ao banco de dados (Neon) com sucesso!');
